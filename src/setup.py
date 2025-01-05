e6a407ee7afe9be831afb06fbe3054b9f001665b
Eyal <eyalb@playstudios-il.com>
2025-01-05 21:18:51 +0200

    trying to fix tests ci/cd
    Signed-off-by: Eyal <eyalb@playstudios-il.com>

--- src/setup.py
+++ src/setup.py
@@ -1,16 +1,18 @@
+import pybind11
 from setuptools import setup, Extension
-from pybind11.setup_helpers import Pybind11Extension, build_ext
+# from pybind11.setup_helpers import Pybind11Extension, build_ext
 
 ext_modules = [
-    Pybind11Extension(
+    Extension(
         "monte_carlo",
         ["src/monte_carlo.cpp"],
+        include_dirs=[pybind11.get_include()],
+        language='c++'
     ),
 ]
 
 setup(
-    name="monte_carlo",
+    name='monte_carlo',
+    version='0.1',
     ext_modules=ext_modules,
-    cmdclass={"build_ext": build_ext},
-    zip_safe=False,
 )
