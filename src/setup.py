289de47847d1b519b3a4b6cbf00c52da8062f827
Eyal <eyalb@playstudios-il.com>
2025-01-05 17:31:40 +0200

    bugfix in setup file after refactoring folder structure

--- src/setup.py
+++ src/setup.py
@@ -4,7 +4,7 @@ from pybind11.setup_helpers import Pybind11Extension, build_ext
 ext_modules = [
     Pybind11Extension(
         "monte_carlo",
-        ["monte_carlo.cpp"],
+        ["src/monte_carlo.cpp"],
     ),
 ]
 
