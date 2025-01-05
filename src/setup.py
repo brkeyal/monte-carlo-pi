787ba65b5ba5a199b20d0ed778f4e6241bdf6dc1
Eyal <eyalb@playstudios-il.com>
2025-01-05 21:20:05 +0200

    Revert "trying to fix tests ci/cd"
    This reverts commit e6a407ee7afe9be831afb06fbe3054b9f001665b.

--- src/setup.py
+++ src/setup.py
@@ -1,18 +1,16 @@
-import pybind11
 from setuptools import setup, Extension
-# from pybind11.setup_helpers import Pybind11Extension, build_ext
+from pybind11.setup_helpers import Pybind11Extension, build_ext
 
 ext_modules = [
-    Extension(
+    Pybind11Extension(
         "monte_carlo",
         ["src/monte_carlo.cpp"],
-        include_dirs=[pybind11.get_include()],
-        language='c++'
     ),
 ]
 
 setup(
-    name='monte_carlo',
-    version='0.1',
+    name="monte_carlo",
     ext_modules=ext_modules,
+    cmdclass={"build_ext": build_ext},
+    zip_safe=False,
 )
